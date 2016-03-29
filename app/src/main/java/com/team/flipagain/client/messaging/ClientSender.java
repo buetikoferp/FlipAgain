83fd297b8aade5746165c9838294a6f4df296e26
Attademo <raffaele.attademo@gmail.com>
2016-03-29 14:13:35 +0200

    ClientServer Klasse hinzugefügt

---
+++ app/src/main/java/com/team/flipagain/client/messaging/ClientSender.java
@@ -0,0 +1,17 @@
+package com.team.flipagain.client.messaging;
+
+import com.team.flipagain.client.application.ApplicationInterface;
+
+/**
+ * Created by Raffaele on 23.03.2016.
+ */
+public class ClientSender implements ApplicationInterface{
+    @Override
+    public boolean login(String username, String password) {
+
+
+
+
+        return false;
+    }
+}
