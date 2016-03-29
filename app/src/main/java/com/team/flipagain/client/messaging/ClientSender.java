44651d85b55efeb51273ccd50b1516b71d852cae
Anthony Delay <adelay@hsr.ch>
2016-03-29 15:52:41 +0200

    Login

--- app/src/main/java/com/team/flipagain/client/messaging/ClientSender.java
+++ app/src/main/java/com/team/flipagain/client/messaging/ClientSender.java
@@ -1,13 +1,17 @@
 package com.team.flipagain.client.messaging;
 
+import android.util.Log;
+
 import com.team.flipagain.client.application.ApplicationInterface;
 
 /**
  * Created by Anthony Delay on 29.03.2016.
  */
 public class ClientSender implements ApplicationInterface {
+    private String TAG = "SENDER";
     @Override
     public boolean getAuthorization(String username, String password) {
-        return false;
+        Log.d(TAG, username + " " + password);
+        return true;
     }
 }
