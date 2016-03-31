b9b3b708e65b6f9d2e62156422fbf32894d7a844
pbuetiko <pbuetiko@hsr.ch>
2016-03-31 13:29:28 +0200

    server.domaininterface & server.dbmanager erstellt

---
+++ app/src/main/java/com/team/flipagain/server/domain/DBManager.java
@@ -0,0 +1,7 @@
+package com.team.flipagain.server.domain;
+
+/**
+ * Created by Philipp on 31.03.2016.
+ */
+public class DBManager {
+}
--- app/src/main/java/com/team/flipagain/server/domain/Placeholder.java
+++
@@ -1,7 +0,0 @@
-package com.team.flipagain.server.domain;
-
-/**
- * Created by Raffaele on 22.03.2016.
- */
-public class Placeholder {
-}
