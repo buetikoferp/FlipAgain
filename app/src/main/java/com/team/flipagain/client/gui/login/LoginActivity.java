c42b6f6c5dccbe2d37c4f956835a4a8e55d3a96c
delay <adelay@hsr.ch>
2016-04-03 22:49:41 +0200

    added some Feature of CreateCard

--- app/src/main/java/com/team/flipagain/client/gui/login/LoginActivity.java
+++ app/src/main/java/com/team/flipagain/client/gui/login/LoginActivity.java
@@ -54,7 +54,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world" , "f@f.f:raffi"
+            "foo@example.com:hello", "bar@example.com:world" , "f@f.f:raffi, test@test.ch:passwort"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
