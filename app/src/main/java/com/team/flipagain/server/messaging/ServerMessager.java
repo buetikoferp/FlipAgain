a533c99e8c8d2c4b5b2479b9eee6bd84fb9562f7
pbuetiko <pbuetiko@hsr.ch>
2016-04-04 15:21:07 +0200

    server.messaging: endpoint, consumer & producer klassen hinzugefügt client.messaging: endpoint, consumer & producer klassen hinzugefügt client.domain: User-Konstruktr erstellt

--- app/src/main/java/com/team/flipagain/server/messaging/ServerMessager.java
+++ app/src/main/java/com/team/flipagain/server/messaging/ServerMessager.java
@@ -1,55 +1,36 @@
 package com.team.flipagain.server.messaging;
 
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.Connection;
-
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
-import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Consumer;
-import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
-import com.team.flipagain.server.application.LoginHandler;
-import com.team.flipagain.server.domain.User;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.concurrent.TimeoutException;
 
 /**
- * Created by Raffaele & Philipp on 30.03.2016.
+ * Created by Philipp on 01.04.2016.
  */
 public class ServerMessager {
 
-    private final static String QUEUE_NAME = "flipChannel";
-
-
-    public static void main(String[] args) throws IOException, InterruptedException, TimeoutException{
-       final FileWriter fos = new FileWriter("C:\\flipagain\\log\\serverlog.txt");
-        ConnectionFactory serverFactory = new ConnectionFactory();
-        serverFactory.setHost("localhost");
-
-        Connection serverConn = serverFactory.newConnection();
-        Channel serverChannel = serverConn.createChannel();
-
-        serverChannel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        System.out.println(" [*] Waiting for messages.");
-
-        Consumer consumer = new DefaultConsumer(serverChannel){
-
-            @Override
-            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException{
-
-                LoginHandler.getAuthorization(null);
-                fos.write((" [x] Received '"+"Hallo" +"'"));
-
-
-
-            }
-        };
-
-        serverChannel.basicConsume(QUEUE_NAME, consumer);
-
+    public void send(Serializable obj){
+        try {
+            ServerProducer sp = new ServerProducer("ServerProducer");
+            sp.sendMessage(obj);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
     }
 
-}
+    public Serializable receive(){
+        try {
+            ServerConsumer sc = new ServerConsumer("ServerConsumer");
+            sc.handleDelivery();
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+}
