6742670db37762d706db9a2c7e62eebeeff1cd8b
pbuetiko <pbuetiko@hsr.ch>
2016-04-05 14:25:06 +0200

    ClientMessager & EndPoint aktualisiert

--- app/src/main/java/com/team/flipagain/client/messaging/ClientMessager.java
+++ app/src/main/java/com/team/flipagain/client/messaging/ClientMessager.java
@@ -1,15 +1,38 @@
 package com.team.flipagain.client.messaging;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.concurrent.TimeoutException;
 
 /**
  * Created by Philipp on 03.04.2016.
  */
-public class ClientMessager extends EndPoint {
+public class ClientMessager{
+    public void send(Serializable obj){
+        ClientProducer cp = null;
+        try {
+            cp = new ClientProducer("flipagain");
+            cp.sendMessage(obj);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
 
-    public ClientMessager(String endpointName) throws IOException, TimeoutException {
-        super(endpointName);
+    }
+
+    public Object receive(){
+        Object obj = null;
+        try {
+            ClientConsumer cc = new ClientConsumer("ClientConsumer");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+
+        return obj;
     }
 
 
